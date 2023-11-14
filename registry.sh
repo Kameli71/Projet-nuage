513eccdb0968b4c4ad45f85d61d9d2754ee64dba
EC2 Default User <ec2-user@ip-10-0-10-134.ec2.internal>
2023-11-14 14:53:17 +0000

    config cluster kub on ec2

--- registry.sh
+++ registry.sh
@@ -9,3 +9,11 @@ docker tag cart localhost:5000/cart
 docker push localhost:5000/cart
 #Visualize image in registry
 #curl -X GET http://localhost:5000/v2/_catalog
+
+#Config and install kub with ansible and roles
+mkdir ansible-kub
+mkdir ansible-kub/roles
+cd ansible-kub/roles
+ansible-galaxy init ec2
+ansible-galaxy init k8s_master
+ansible-galaxy init k8s_slave
